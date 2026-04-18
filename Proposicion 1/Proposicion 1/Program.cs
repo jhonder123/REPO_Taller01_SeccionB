9da3a134a2eaf5ec6d09f19133144dfee97194c0
jhonder123 <jhonder.abreu21@gmail.com>
2026-04-18 15:02:46 -0400

    Initial commit

---
+++ Proposicion 1/Proposicion 1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Jhonder Abreu
+ * Fecha: 18/4/2026
+ * Hora: 2:50 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Proposicion_1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
